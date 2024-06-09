1659fc55a7fb11ec432def38b3c7849ac762e49c
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-09 16:50:36 +0700

    signin activity

--- app/src/main/java/com/example/thejournalapp/MainActivity.java
+++ app/src/main/java/com/example/thejournalapp/MainActivity.java
@@ -1,17 +1,28 @@
 package com.example.thejournalapp;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseNetworkException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -19,6 +30,10 @@ public class MainActivity extends AppCompatActivity {
     private EditText signInEmail, signInPwd;
     private Button signInBtn, signUpAnAccount;
 
+    // firebase authentication
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +51,35 @@ public class MainActivity extends AppCompatActivity {
         signInBtn = findViewById(R.id.sign_in_btn);
         signUpAnAccount = findViewById(R.id.sign_up_account_btn);
 
+        firebaseAuth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                currentUser = firebaseAuth.getCurrentUser();
+
+                // CHeck the user if logged in or not
+                if (currentUser != null) {
+                    // user Already logged in
+                } else {
+                    // The user signed out
+                }
+            }
+        };
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = signInEmail.getText().toString().trim();
+                String pwd = signInPwd.getText().toString().trim();
+
+                if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd)) {
+                    signInEmailAndPwd(email, pwd);
+                } else {
+                    Toast.makeText(MainActivity.this, "No Empty Fields are Allowed!", LENGTH_SHORT).show();
+                }
+            }
+        });
+
         signUpAnAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,5 +87,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void signInEmailAndPwd(String email, String pwd) {
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(pwd)) {
+            firebaseAuth.signInWithEmailAndPassword(email, pwd)
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            Intent intent = new Intent(MainActivity.this, JournalListActivity.class);
+                            startActivity(intent);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), LENGTH_SHORT).show();
+                        }
+                    });
+        }
     }
 }
