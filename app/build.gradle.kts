9427632131e7f043c9a3421e5a10a384e9e6eb00
MeansKane <phamminhkhangpy2003@gmail.com>
2024-06-09 14:49:37 +0700

    ....

---
+++ app/build.gradle.kts
@@ -0,0 +1,59 @@
+plugins {
+    alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = "com.example.thejournalapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.thejournalapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+
+    implementation("com.google.firebase:firebase-analytics")
+
+    // fire store
+    implementation("com.google.firebase:firebase-firestore")
+
+    // authentication
+    implementation("com.google.firebase:firebase-auth")
+
+    // storage
+    implementation("com.google.firebase:firebase-storage")
+
+    // glide
+    implementation ("com.github.bumptech.glide:glide:4.14.2")
+    // Skip this if you don't want to use integration libraries or configure Glide.
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.14.2")
+}
